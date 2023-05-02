b0e4af10f1eefd1e500496493cf346a9048d62b9
tenzindala2000@gmail.com <tenzindala2000@gmail.com>
2023-05-01 23:15:20 -0400

    Completed the Routing Module

--- src/genres/CreateGenre.tsx
+++ src/genres/CreateGenre.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function CreateGenre() {
+  const navigation = useNavigate()
   return (
     <>
     <h3>creating Genre</h3>
+    <button className='btn btn-primary' onClick={() => {
+      navigation('/genres')
+    }}>Save Changes</button>
     </>
   )
 }
--- src/genres/EditGenre.tsx
+++ src/genres/EditGenre.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function EditGenre() {
+  const {id} :any = useParams()
   return (
+    <>
     <div>Edit Genre</div>
+    The id is {id}
+    </>
   )
 }
