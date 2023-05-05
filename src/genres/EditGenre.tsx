04456a95540f2f38417846b03383589ff7054ed5
tenzindala2000@gmail.com <tenzindala2000@gmail.com>
2023-05-05 09:26:14 -0400

    Adding Edit Genre

--- src/genres/EditGenre.tsx
+++ src/genres/EditGenre.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import GenreForm from './GenreForm';
 
 export default function EditGenre() {
   const {id} :any = useParams()
   return (
     <>
     <div>Edit Genre</div>
-    The id is {id}
+    <GenreForm model = {{name : "Action"}} onSubmit={async value => {
+        await new Promise(r => setTimeout(r,1));
+        console.log(id)
+        console.log(value)
+      }}/>
+
     </>
   )
 }
