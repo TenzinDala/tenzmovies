455732679a95703d63a0ddb0edeab7dd28f8057f
tenzindala2000@gmail.com <tenzindala2000@gmail.com>
2023-05-07 11:34:16 -0400

    Added Forms to Movie Theaters Page

--- src/movietheaters/EditMovieTheaters.tsx
+++ src/movietheaters/EditMovieTheaters.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import MovieTheaterForm from './MovieTheaterForm'
 
 export default function EditMovieTheaters() {
   return (
+    <>
     <div>EditMovieTheaters</div>
+    <MovieTheaterForm 
+      model={{name: "Tenzin"}}
+      onSubmit={values => console.log(values)}
+    />
+    </>
   )
 }
